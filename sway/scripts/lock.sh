d9d8e1a68fcc2fc9e7f3a784626eeab3fca75ab2
Gabriel Lima <lima.a.gabriel@icloud.com>
2024-03-22 12:11:40 -0300

    turn off monitors on lock

--- sway/scripts/lock.sh
+++ sway/scripts/lock.sh
@@ -3,6 +3,10 @@
 STATUS=$(playerctl status);
 	
 [[ $STATUS == "Playing" ]] && playerctl pause;
+swaymsg "output * power off"
+
 [ -z "$1" ] && swaylock -K -u -c 282828 || swaylock -f -K -u -c 282828;
 [ ! -z "$1" ] && eval $1;
+
+swaymsg "output * power on"
 [ -z "$1" ] && [[ $STATUS == "Playing" ]] && playerctl play;
